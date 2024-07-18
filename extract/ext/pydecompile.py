24a2ae9d686048f3c74fbb1143842d71d262a8b8
cccs-ml <127332022+cccs-ml@users.noreply.github.com>
2024-07-18 14:53:53 +0000

    fix: raise Invalid pyc on xdis assert fails

--- extract/ext/pydecompile.py
+++ extract/ext/pydecompile.py
@@ -82,6 +82,11 @@ def decompile_pyc(filepath: str) -> str:
         # likely an incorrectly or unimplemented code by uncompyle:
         # bad marshal data (unknown type code)
         raise Invalid
+    except AssertionError:
+        # `xdis` has multiple `assert`s to validate that the code it is generating make sense.
+        # if one of these `assert`s fails, then chances are the pyc was corrupt, malformed, protected
+        # or there's a bug with `xdis`' parsing.
+        raise Invalid
     finally:
         sys.stdout = stdout
         sys.stderr = stderr
